e0bcf6f9f4a44813c00ebff1a1f086fb8cd9701c
kristavan <vanderhorst.20@wright.edu>
2017-04-08 15:48:21 -0400

    Update folder structure
    Updated folder structure and renamed home to homepage to avoid python
    naming conflicts.

--- hq/hq/urls.py
+++ hq/hq/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from home.views import *
+from homepage.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
