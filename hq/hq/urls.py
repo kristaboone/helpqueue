efc8e0b8f7c45d373efc63d2044697f0ea203932
John Klaassen <jogaklaa@gmail.com>
2017-04-08 13:13:54 -0400

    Add initial bootstrap homepage
    WIP

--- hq/hq/urls.py
+++ hq/hq/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from home.views import *
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', HomeView.as_view()),
+
 ]
