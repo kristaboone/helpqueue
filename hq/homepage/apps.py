1e482c364ae11bf7d3d70ad186df3ced1162929f
kristavan <vanderhorst.20@wright.edu>
2017-04-08 16:08:20 -0400

    Finish merging files

--- hq/homepage/apps.py
+++ hq/homepage/apps.py
@@ -1,4 +1,4 @@
 from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
-	name = 'homepage'
+    name = 'home'
--- hq/homepage/models.py
+++ hq/homepage/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+# from .utils import code_generator, create_shortcode
+from django.conf import settings
+from django.contrib.auth.models import User, Group
+
+
+class Class(models.Model):
+    className = models.CharField(max_length=50,)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+    semesterEndDate = models.DateField()
+    students = models.ManyToManyField(User)
+    # instructor = models.ForeignKey(User)
